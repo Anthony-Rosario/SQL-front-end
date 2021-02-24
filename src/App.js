49447dc05f48730d936dcfed36e5cbf031207687
Anthony Rosario <anthonymrosario225@gmail.com>
2021-02-24 15:22:36 -0800

    made basic layout of app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import HomePage from './HomePage.js';
+import ListPage from './ListPage.js';
+import DetailPage from './DetailPage.js';
+import CreatePage from './CreatePage.js';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route 
+          path="/" 
+          exact
+          components = { HomePage }
+          />
+         
+          <Route 
+          path="/skateboards" 
+          exact
+          component={ ListPage }
+          />
+          
+          <Route 
+          path="/skateboards/:id" 
+          exact
+          component={ DetailPage }
+          />
+          
+          <Route 
+          path="/create" 
+          exact
+          component={ CreatePage }
+          />
+          
+        </Switch>
+      </Router>
     </div>
   );
 }
-
-export default App;
